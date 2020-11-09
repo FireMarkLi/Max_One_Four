74112c09006de09a940be3bebfc8c5666714d216
wanyaowei <1718707182@qq.com>
2020-11-09 14:51:31 +0800

    132

---
+++ Doctor_MaxOne_Four/wanyaowei/wan.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Doctor_MaxOne_Four.wanyaowei
+{
+    public class wan
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Set { get; set; }
+    }
+}
